babc327c7d1b3f328773e154d63b3272c52493cf
TheusMarqs <matheusmarqsilva@gmail.com>
2023-04-04 17:08:49 -0300

    Operações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CalcComponent } from './calc/calc.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CalcComponent } from './calc/calc.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -9,7 +9,23 @@ export class CalcComponent {
   num1 : number = 0;
   num2 : number = 0;
   result : number = 0;
-  calc(){
-    this.result = this.num1 + this.num2;
+  calc(op : string){
+    switch (op) {
+      case '+':
+        this.result = this.num1 + this.num2;
+        break;
+
+      case '-':
+        this.result = this.num1 - this.num2;
+        break;
+
+      case '*':
+        this.result = this.num1 * this.num2;
+        break;
+
+      case '/':
+        this.result = this.num1 / this.num2;
+        break;
+    }
   }
 }
